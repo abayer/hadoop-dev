fe51e6962257dddc102d9b89898446d87dad25f4
Eli Collins <eli@cloudera.com>
2011-09-13 22:16:02 -0700

    Use the Linux task controller

--- run.sh
+++ run.sh
@@ -1,21 +1,55 @@
 #!/usr/bin/env bash
 
-HADOOP_HOME=/home/eli/src/cloudera/hadoop1
+HADOOP_VERSION=hadoop-0.20.207.0-SNAPSHOT
+HADOOP_SRC=/home/eli/src/cloudera/hadoop1
+DEPLOY_BASE=/deploy
+HADOOP_HOME=$DEPLOY_BASE/$HADOOP_VERSION
+USER=eli
 
 . util.sh
 
-#exec_all eli stop
-#exit
+# Create and deploy hadoop configuration
+deploy_hadoop $USER
 
-#create_confs eli
-#format_nn
-#exit
+# Initialize and start the file system
+run_hdfs_cmd namenode -format
 
-exec_all eli start
-run_cmd dir-nn dfsadmin -safemode leave
+exec_hdfs start
 
-run_cmd dir-nn fs -lsr /
+run_hdfs_cmd dfsadmin -safemode leave
 
-J=$HADOOP_HOME/build/hadoop-examples-*.jar
-run_cmd dir-nn jar $J pi -Dmapred.map.tasks=3 2 10
+run_hdfs_cmd fs -chown hdfs:hadoop /
 
+run_hdfs_cmd fs -mkdir /user/$USER
+run_hdfs_cmd fs -chmod 755 /user/$USER
+run_hdfs_cmd fs -chown $USER:$USER /user/$USER
+
+run_hdfs_cmd fs -mkdir /tmp
+run_hdfs_cmd fs -chmod 777 /tmp
+
+run_hdfs_cmd fs -mkdir /mapred/system
+run_hdfs_cmd fs -chmod 755 /mapred/system
+run_hdfs_cmd fs -chown -R mapred:hadoop /mapred
+
+# Create a file as a normal user
+tmp=`mktemp`
+run_cmd fs -put $tmp $(basename $tmp)
+run_cmd fs -lsr /
+
+# Start MR and run a job
+exec_mr start
+
+J=$HADOOP_HOME/hadoop-examples-*.jar
+run_cmd jar $J pi -Dmapred.map.tasks=3 2 10
+
+# Restart the daemons and run the job again
+exec_mr stop
+exec_hdfs stop
+exec_hdfs start
+run_hdfs_cmd dfsadmin -safemode leave
+exec_mr start
+
+run_cmd jar $J pi -Dmapred.map.tasks=3 2 10
+
+exec_mr stop
+exec_hdfs stop
--- util.sh
+++ util.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
 #
-# Create conf dirs for each pseudo "host"
+# Unpack the tarball to the deployment dir and create 
+# conf dirs for each pseudo "host".
 #
-function create_confs () {
+function deploy_hadoop () {
     local user=$1
+    # Create the deployment dir
+    pushd $HADOOP_SRC
+    ant tar -Djava5.home=$JAVA5_HOME -Dforrest.home=$FORREST_HOME
+    popd
+    if [ -d "$HADOOP_HOME" ]; then
+        sudo rm -rf $HADOOP_HOME
+    fi
+    sudo chown -R $user:$user $DEPLOY_BASE
+    tar -xzf $HADOOP_SRC/build/$HADOOP_VERSION.tar.gz -C $DEPLOY_BASE
+    # Create dirs and configs for each pseudo host
     for dir in dir-nn dir-2nn dir-jt dir-s1 dir-s2 dir-s3
     do
         dir_path=$HADOOP_HOME/$dir
@@ -13,15 +24,41 @@ function create_confs () {
         fi
         mkdir $dir_path
         cp -r conf/20x $dir_path/conf
-        # xx use HADOOP_HOME 
-        sed -i "s/_ROOT_/\/home\/eli\/src\/cloudera\/hadoop1\/$dir/g" \
+        # xx Use HADOOP_HOME 
+        sed -i "s/_ROOT_/\/deploy\/$HADOOP_VERSION\/$dir/g" \
             $dir_path/conf/*
         sed -i "s/_USER_/$user/g" $dir_path/conf/*
-        for d in tmp logs pids dfs mapred
+        sudo chown root:hadoop $dir_path/conf
+        for d in bin tmp logs pids dfs mapred
         do
             mkdir $dir_path/$d
+            sudo chgrp -R hadoop $dir_path/$d
+            sudo chmod g+w $dir_path/$d
         done
+        sudo chown hdfs:hadoop $dir_path/dfs
+        sudo chown mapred:hadoop $dir_path/mapred
+        # Build and deploy the task-controller for each host since
+        # the path to the config file is baked into the binary and
+        # each config file contains host-specif paths.
+        if [ "$dir" = "dir-s1" ] ||
+           [ "$dir" = "dir-s2" ] ||
+           [ "$dir" = "dir-s3" ]; then
+            pushd $HADOOP_HOME
+            ant task-controller -Dhadoop.conf.dir=$dir_path/conf
+            cp ./build/$HADOOP_VERSION/bin/task-controller $dir_path/bin
+            sudo chown root:mapred $dir_path/bin/task-controller
+            sudo chmod 6050 $dir_path/bin/task-controller
+            sudo chown root:mapred $dir_path/conf/taskcontroller.cfg
+            sudo chmod 0400 $dir_path/conf/taskcontroller.cfg
+            popd
+        fi
+        sudo chown root:hadoop $dir_path
     done
+    # Paths including and leading up to the directories listed
+    # in mapred.local.dir and hadoop.log.dir need to be owned by 
+    # root and have 755 perms.
+    sudo chown root:hadoop $HADOOP_HOME
+    sudo chown root:hadoop $DEPLOY_BASE
 }
 
 #
@@ -48,34 +85,40 @@ function run_daemon () {
             done
         fi
     fi
-    $HADOOP_HOME/bin/hadoop-daemon.sh --config $conf $cmd $daemon
+    sudo -u $user $HADOOP_HOME/bin/hadoop-daemon.sh --config $conf $cmd $daemon
 }
 
 #
-# Run a hadoop command with a given conf dir
+# Run a hadoop command
 #
 function run_cmd () {
-    local dir=$1
-    shift
+    local dir="dir-nn"
     HADOOP_CONF_DIR=$HADOOP_HOME/$dir/conf $HADOOP_HOME/bin/hadoop $*
 }
 
-function format_nn () {
-    run_cmd dir-nn namenode -format
+function run_hdfs_cmd () {
+    local dir="dir-nn"
+    HADOOP_CONF_DIR=$HADOOP_HOME/$dir/conf sudo -E -u hdfs \
+        $HADOOP_HOME/bin/hadoop $*
 }
 
 #
 # Run the given command on all the daemons
 #
-function exec_all () {
-    local user=$1
-    local cmd=$2
-    run_daemon $user $cmd namenode dir-nn
-    run_daemon $user $cmd jobtracker dir-jt
-    run_daemon $user $cmd datanode dir-s1
-    run_daemon $user $cmd datanode dir-s2
-    run_daemon $user $cmd datanode dir-s3
-    run_daemon $user $cmd tasktracker dir-s1
-    run_daemon $user $cmd tasktracker dir-s2
-    run_daemon $user $cmd tasktracker dir-s3
+function exec_hdfs () {
+    local cmd=$1
+    run_daemon hdfs $cmd namenode dir-nn
+    run_daemon hdfs $cmd datanode dir-s1
+    run_daemon hdfs $cmd datanode dir-s2
+    run_daemon hdfs $cmd datanode dir-s3
 }
+
+function exec_mr () {
+    local cmd=$1
+    run_daemon mapred $cmd jobtracker dir-jt
+    run_daemon mapred $cmd tasktracker dir-s1
+    run_daemon mapred $cmd tasktracker dir-s2
+    run_daemon mapred $cmd tasktracker dir-s3
+}
+
+
