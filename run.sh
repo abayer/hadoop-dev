50f4dfa6b982b203ca8367cc7aa2f21bba8fa874
Andrew Bayer <andrew.bayer@gmail.com>
2011-10-04 14:52:13 -0700

    Getting rid of hardcoded vars

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-HADOOP_VERSION=hadoop-0.20.206.0-SNAPSHOT
-HADOOP_SRC=/home/eli/src/cloudera/hadoop1
-HADOOP_TARBALL=$HADOOP_SRC/build/$HADOOP_VERSION.tar.gz
-DEPLOY_BASE=/deploy
+HADOOP_VERSION=${HADOOP_VERSION:-hadoop-0.20.206.0-SNAPSHOT}
+HADOOP_SRC=${HADOOP_SRC:-/home/eli/src/cloudera/hadoop1}
+HADOOP_TARBALL=${HADOOP_TARBALL:-$HADOOP_SRC/build/$HADOOP_VERSION.tar.gz}
+DEPLOY_BASE=${DEPLOY_BASE:-/deploy}
 HADOOP_HOME=$DEPLOY_BASE/$HADOOP_VERSION
-USER=eli
+USER=${USER:-eli}
 
 . util.sh
 
